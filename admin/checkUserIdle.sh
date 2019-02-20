15cb5a83910184b9382cc219c0de7ff458bf1c06
Howard <howard@renci.org>
2019-02-20 16:27:06 -0500

    Reviewd with more comments, but NOT tested

--- admin/checkUserIdle.sh
+++ admin/checkUserIdle.sh
@@ -1,14 +1,32 @@
 #! /bin/bash -x
+
+# Important note:  we need to make sure this code does not take longer to run than
+# the interval at which we are running the script.  This consideration may effect how
+# we run the script
+
 # $1 is the user to check, $2 is the max idle time in seconds, $3 is the minimum amount of CPU
 # utilization to be considered active
 #debug=echo
 debug=
-spaceDirectory="/home/efs/userSpaces"
-lastActiveFile="${spaceDirectory}/.lastActive-${1}"
-userHome="${spaceDirectory}/${1}_space"
+
+# Store the arguments in somewhat more obvious variable names
 theUser=$1
 maxIdle=$2
 minActiveCPU=$3
+
+# The directory containing the user spaces.  AWS specific
+spaceDirectory="/home/efs/userSpaces"
+
+# the lastActiveFile contains the time in seconds since the epoch
+# that the user was last known to be "active"
+lastActiveFile="${spaceDirectory}/.lastActive-${theUser}"
+
+# Calulate the user's home directory
+userHome="${spaceDirectory}/${theUser}_space"
+
+# This calls an API written by Murali that invokes the amazon api to stop the machine
+# That API will have to be re-written for GCP
+killCommand="/usr/bin/curl http://18.205.3.38:8000/api/machines/stop/"
 
 # Set the field seperator
 IFS=$'\n'
@@ -26,16 +44,18 @@ if [ ! -f $lastActiveFile ]; then
 fi
 
 # We are going to check both file and CPU activity as measures of idleness.  Anytime we detect the user
-# not being idle, we want to be sure that the timestamp in the lastActiveFile is updated.
+# is active, we want to be sure that the timestamp in the lastActiveFile is updated.
 
 # Check for the date of the last file modification
 lastFileTime=( $(find $userHome -type f -printf '%T@ %p\n' | sort -n | tail -1 | awk {'print $1}'))
+
+# Convert the lastFileTime to an integer value
 lastIntFileTime=${lastFileTime%.*}
-echo $lastIntFileTime
-echo $testDate
 lastActiveDate=`cat $lastActiveFile`
+
+# Calculate how long since the last file was changed
 timeSinceLastFile=$(echo "$testDate - $lastIntFileTime"|/usr/bin/bc)
-echo $timeSinceLastFile
+#echo $timeSinceLastFile
 
 # Is the time since the last file update less than the maxIdle time.  If so the user is active
 userActive=$(echo "$timeSinceLastFile > $maxIdle"|/usr/bin/bc)
@@ -45,8 +65,8 @@ if [ $userActive !=  0 ]; then
    exit
 fi
 
-# They were't active from the file system point of view, but they may yet be active from the CPU
-# point of view.  We check that next.
+# If we get here the user wasn't active from the file system point of view, but they may be active 
+# from the CPU utilization point of view.  We check that next.
 
 # get an array of all process for the specified user. The first two grep -v commands eliminate pam 
 # processes and the last one eliminates the jupterhub process.
@@ -57,17 +77,20 @@ if (( $numActiveProcs < 1 )); then
    # The user has no active processes, have they exceeded max idle
    lastActiveDate=`cat $lastActiveFile`
    idleTime=$(($testDate-$lastActiveDate))
-   echo $idleTime
+#  echo $idleTime
   
    if (( $idleTime > $maxIdle )); then
      echo "resource is idle"
      /bin/rm $lastActiveFile
+
+      # Stop the instance
+     $killCommand
+     exit 0
    fi
-   # Stop the instance
-   exit 0
 fi
 
-# Next step: how much CPU is being used. Find the CPU utilization of each user process
+# Next step: There are user processes: how much CPU is being used? 
+# Find the CPU utilization of each user process
 # and sum it up.  If the sum is less than the minActiveCPU, the user is idle. Otherwise
 # we update the lastActiveFile
 totalCPU=0.0
@@ -75,14 +98,20 @@ totalCPU=0.0
 # For each process, get it's pid, then find how much cpu it's using
 for i in "${processList[@]}";
 do 
+   # Field one in the process list is the pid
    thisPid=`/bin/echo "$i" | /usr/bin/awk '{print $1}'`
+
+   # Call top to get the cpu usage for each process
    thisCPUUsage=`/usr/bin/top -b -n 1 -p $thisPid | /bin/grep $thisPid | /usr/bin/awk '{print $9}'`
    if [ -z "$thisCPUUsage" ]; then
       thisCPUUsage=0.0
    fi
+
+   # Add the cpu usage for this process to the cumulative total of all processes
    totalCPU=$(echo "$totalCPU + $thisCPUUsage"|/usr/bin/bc)
 done
 
+# User is active if the totalCPU usage is more that the min active usage
 userActive=$(echo "$totalCPU > $minActiveCPU"|/usr/bin/bc)
 if [ $userActive !=  0 ]; then
    # The user is active. 
@@ -100,7 +129,9 @@ else
      # the resource doesn't get immediately shut down the next time we run the script.
      /bin/rm $lastActiveFile
 
-     # Here's where we issue the call to stop the resource.
+     # Issue the call to stop the resource.
+     $killCommand
+     exit 0
    fi
 fi
 
